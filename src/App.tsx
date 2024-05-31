fe93393a09dcda5df53cf22ffaf70b0193f05470
nedim begic <nedim.begic93@gmail.com>
2024-05-31 16:38:46 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import CarouselComponent from "./components/Carousel";
+
+function App() {
+  return (
+    <>
+      <CarouselComponent />
+    </>
+  );
+}
+
+export default App;
